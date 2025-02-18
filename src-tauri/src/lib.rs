1da593acd8ae5d596c366cf6389e17563187b06a
Khoshbin <xoshbin@gmail.com>
2025-02-18 20:57:10 +0300

    Add system tray functionality

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+pub mod tray;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -9,6 +10,12 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(setup_app)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn setup_app(app: &mut tauri::App) -> Result<(), Box<dyn std::error::Error>> {
+    tray::setup_tray(app)?;
+    Ok(())
 }
---
+++ src-tauri/src/tray.rs
@@ -0,0 +1,31 @@
+use tauri::{
+    menu::{Menu, MenuItem}, tray::TrayIconBuilder, App, Manager
+};
+
+pub fn setup_tray(app: &App) -> Result<(), Box<dyn std::error::Error>> {
+    let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+    let settings_i = MenuItem::with_id(app, "settings", "Settings", true, None::<&str>)?;
+    let menu = Menu::with_items(app, &[&quit_i, &settings_i])?;
+
+    let _tray = TrayIconBuilder::new()
+        .icon(app.default_window_icon().unwrap().clone())
+        .menu(&menu)
+        .on_menu_event(|app, event| match event.id.as_ref() {
+            "quit" => {
+                println!("quit menu item was clicked");
+                app.exit(0);
+            }
+            "settings" => {
+                println!("settings menu item was clicked");
+                let settings_window = app.get_webview_window("settings").unwrap(); // 'settings' is the label from tauri.conf.json
+                settings_window.show().unwrap();
+                settings_window.set_focus().unwrap();
+            }
+            _ => {
+                println!("menu item {:?} not handled", event.id);
+            }
+        })
+        .build(app)?;
+
+    Ok(())
+}
