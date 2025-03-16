d6ec84a62b4a11cd1e6a782de49a3a4a63179044
Khoshbin <xoshbin@gmail.com>
2025-03-17 00:02:40 +0300

    refactor(services): update service exports to named exports for consistency

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,8 @@
 // Export all services
-export { default as logService } from "./logService";
+export { logService } from "./logService";
 export { default as extensionManager } from "./extensionManager";
-export { default as settingsService } from "./settingsService";
-export { default as clipboardService } from "./clipboardHistoryService";
-export { default as actionService } from "./actionService";
+export { settingsService } from "./settingsService";
+export { ClipboardHistoryService as clipboardService } from "./clipboardHistoryService";
+export { actionService } from "./actionService";
 export { default as commandService } from "./commandService";
 // Add other services as needed
