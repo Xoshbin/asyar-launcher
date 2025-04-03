bef547e3b1d9d656066c228f1f9c47ec3680d9fc
Khoshbin <xoshbin@gmail.com>
2025-04-03 13:40:55 +0300

    fix: VS Code complain about "Cannot find module" for svelte

--- svelte.config.js
+++ svelte.config.js
@@ -9,6 +9,18 @@ const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
+    alias: {
+      // this will match a file
+      // "my-file": "path/to/my-file.js",
+
+      // this will match a directory and its contents
+      // (`my-directory/x` resolves to `path/to/my-directory/x`)
+      "src": "./src",
+
+      // an alias ending /* will only match
+      // the contents of a directory, not the directory itself
+      // "my-directory/*": "path/to/my-directory/*",
+    },
   },
 };
 
